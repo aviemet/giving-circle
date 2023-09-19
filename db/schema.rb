08c39187918f23c5003f83ad640e71bb066cb807
Avram Walden <aviemet@gmail.com>
2023-09-19 11:08:26 -0700

    Finalizes updates

--- db/schema.rb
+++ db/schema.rb
@@ -82,22 +82,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_21_144508) do
     t.index ["slug"], name: "index_themes_on_slug", unique: true
   end
 
-  create_table "contacts", force: :cascade do |t|
-    t.string "contactable_type", null: false
-    t.bigint "contactable_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["contactable_type", "contactable_id"], name: "index_contacts_on_contactable"
-  end
-
-  create_table "members", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
