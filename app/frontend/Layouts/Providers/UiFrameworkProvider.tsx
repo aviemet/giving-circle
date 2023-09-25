74b0b63fbc54b86a75dd694173ac2f155bcd5474
Avram Walden <aviemet@gmail.com>
2023-09-25 11:26:45 -0700

    Gets postcss working

--- app/frontend/Layouts/Providers/UiFrameworkProvider.tsx
+++ app/frontend/Layouts/Providers/UiFrameworkProvider.tsx
@@ -1,17 +1,7 @@
 import React from 'react'
 import { MantineProvider, createTheme } from '@mantine/core'
 import { Notifications } from '@mantine/notifications'
-
-export const breakpoints: Record<string, string> = {
-	'hd': '120rem', // 1920px,
-	'2xl': '110rem', // 1760px,
-	xl: '90rem', // 1440px, default 88rem
-	lg: '80rem', // 1280px, default 75rem
-	md: '62rem',
-	sm: '48rem',
-	xs: '36rem',
-	'2xs': '30rem', // 480px
-}
+import breakpoints from './breakpoints.mjs'
 
 export const theme = createTheme({
 	...breakpoints,
---
+++ app/frontend/Layouts/Providers/breakpoints.mjs
@@ -0,0 +1,7 @@
+export default {
+	xs: '30em',
+	sm: '48em',
+	md: '64em',
+	lg: '74em',
+	xl: '90em',
+}
