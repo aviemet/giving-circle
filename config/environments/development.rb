9c5adde158aa69a97607f025345bd068c4c89eef
Avram Walden <aviemet@gmail.com>
2024-07-07 10:55:37 -0700

    fix: asset digests false

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Prevent Rails from making asset digests, handled by Vite
+  config.assets_digest = false
+
   # Suppress logger output for asset requests.
   # config.assets.quiet = true
 
