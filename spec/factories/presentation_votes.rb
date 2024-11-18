f83e22ecc03cc47083d986da2075c004e371694c
Avram Walden <aviemet@gmail.com>
2024-11-18 14:54:56 -0800

    fix: annotates models

--- spec/factories/presentation_votes.rb
+++ spec/factories/presentation_votes.rb
@@ -2,11 +2,20 @@
 #
 # Table name: presentation_votes
 #
-#  id         :uuid             not null, primary key
-#  name       :string
-#  type       :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :uuid             not null, primary key
+#  name            :string
+#  vote_type       :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  presentation_id :uuid
+#
+# Indexes
+#
+#  index_presentation_votes_on_presentation_id  (presentation_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (presentation_id => presentations.id)
 #
 FactoryBot.define do
   factory :presentation_vote do
