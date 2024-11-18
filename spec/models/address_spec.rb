ee85a590b62e6cffd4a8dd06e1ae60956b90a82f
Avram Walden <aviemet@gmail.com>
2024-11-18 14:59:58 -0800

    chore: annotates address spec

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id         :uuid             not null, primary key
+#  address    :string
+#  address_2  :string
+#  city       :string
+#  country    :string
+#  postal     :string
+#  region     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  contact_id :uuid             not null
+#
+# Indexes
+#
+#  index_addresses_on_contact_id  (contact_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (contact_id => contacts.id)
+#
 # spec/models/address_spec.rb
 
 require 'rails_helper'
