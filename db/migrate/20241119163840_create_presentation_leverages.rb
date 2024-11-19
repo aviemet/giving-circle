1a28c7d27c99a1455a01155650f02ca077d61e32
Avram Walden <aviemet@gmail.com>
2024-11-19 09:02:21 -0800

    fix: adds template field

--- db/migrate/20241119163840_create_presentation_leverages.rb
+++ db/migrate/20241119163840_create_presentation_leverages.rb
@@ -3,6 +3,7 @@ class CreatePresentationLeverages < ActiveRecord::Migration[7.1]
     create_table :presentation_leverages, id: :uuid do |t|
       t.string :name
       t.integer :type
+      t.boolean :template, default: false, null: false
 
       t.timestamps
     end
