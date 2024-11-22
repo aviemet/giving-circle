b3b366bc58e6c8e356509ccc18f7592b2c61ec06
Avram Walden <aviemet@gmail.com>
2024-11-22 10:08:33 -0800

    fix: working on condensing file input files

--- app/frontend/Components/Inputs/FileInput/index.tsx
+++ app/frontend/Components/Inputs/FileInput/index.tsx
@@ -1,32 +1,61 @@
-import React, { forwardRef } from 'react'
-import { type FileInputProps as MantineFileInputProps } from '@mantine/core'
+import React, { forwardRef, useState } from 'react'
+import { FileInput, type FileInputProps as MantineFileInputProps } from '@mantine/core'
 import { type BaseInputProps } from '..'
 import Label from '../Label'
 import InputWrapper from '../InputWrapper'
 import SingleFileInputComponent from './Single'
 import MultiSelectComponent from '../MultiSelect'
 
-export interface FileInputProps extends MantineFileInputProps, BaseInputProps {}
+export interface FileInputProps<M extends boolean = false> extends MantineFileInputProps<M>, BaseInputProps  {
+	multiple: M
+	clearable?: boolean
+}
 
 const FileInputComponent = forwardRef<HTMLButtonElement, FileInputProps>((
-	{ multiple = false, id, ...props },
+	{
+		id,
+		label,
+		required = false,
+		size = 'md',
+		wrapper,
+		wrapperProps,
+		clearable = false,
+		value,
+		onChange,
+		readOnly,
+		disableAutofill = false,
+		multiple = false,
+		...props
+	},
 	ref,
 ) => {
 	const inputId = id || props.name
 
-	const FileInput = multiple ? MultiSelectComponent : SingleFileInputComponent
+	// Manage value as a local state to enable clearable feature
+	const [localValue, setLocalValue] = useState<File | File[] | null>(null)
+
+	const handleChange = (payload: File | File[] | null) => {
+		if(onChange) {
+			onChange(payload)
+		} else {
+			setLocalValue(payload)
+		}
+	}
 
 	return (
-		<InputWrapper wrapper={ props.wrapper } wrapperProps={ props.wrapperProps }>
-			{ props.label && <Label required={ props.required } htmlFor={ inputId }>
-				{ props.label }
+		<InputWrapper wrapper={ wrapper } wrapperProps={ wrapperProps }>
+			{ label && <Label required={ required } htmlFor={ inputId }>
+				{ label }
 			</Label> }
-			<FileInput ref={ ref } id={ inputId } { ...props } />
-			{ /* { multiple ?
-				<MultiSelectComponent ref={ ref } id={ inputId } { ...props } />
-				:
-				<SingleFileInputComponent ref={ ref } id={ inputId } { ...props } />
-			} */ }
+			<FileInput
+				ref={ ref }
+				multiple={ false }
+				value={ localValue }
+				onChange={ handleChange }
+				required={ required }
+				size={ size }
+				{ ...props }
+			/>
 		</InputWrapper>
 	)
 })
