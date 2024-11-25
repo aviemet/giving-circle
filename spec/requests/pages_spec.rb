8347034ae6f0fa034e0429c4a6e7dc09fef47df9
Avram Walden <aviemet@gmail.com>
2024-11-25 07:44:33 -0800

    test: pages tests pass

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,7 +1,36 @@
 require 'rails_helper'
+require_relative '../support/devise'
 
 RSpec.describe "Pages", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /" do
+
+    context "when logged in as an admin" do
+      login_super_admin
+
+      it "renders a successful response" do
+        get root_url
+
+        expect(response).to be_successful
+      end
+    end
+
+    context "when logged in as a normal user" do
+      login_user(:admin)
+
+      it "renders a successful response" do
+        get root_url
+
+        expect(response).to be_successful
+      end
+    end
+
+    context "without being logged in" do
+      it "renders a successful response" do
+        get root_url
+
+        expect(response).to be_successful
+      end
+    end
+
   end
 end
